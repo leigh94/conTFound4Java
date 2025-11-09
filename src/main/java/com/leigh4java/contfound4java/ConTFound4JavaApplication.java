c440f11b0acfb137b545a24b470df398a7dee808
李磊 <lei_leigh@163.com>
2025-11-09 16:44:41 +0800

    Initial commit

---
+++ src/main/java/com/leigh4java/contfound4java/ConTFound4JavaApplication.java
@@ -0,0 +1,13 @@
+package com.leigh4java.contfound4java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConTFound4JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConTFound4JavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leigh4java/contfound4java/ConTFound4JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leigh4java.contfound4java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConTFound4JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
