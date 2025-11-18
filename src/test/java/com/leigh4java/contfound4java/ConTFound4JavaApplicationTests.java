24cf7d410cb1679930974d6c57ac63e8fabd66a9
李磊 <lei_leigh@163.com>
2025-11-18 10:48:15 +0800

    add test

--- src/test/java/com/leigh4java/contfound4java/ConTFound4JavaApplicationTests.java
+++ src/test/java/com/leigh4java/contfound4java/ConTFound4JavaApplicationTests.java
@@ -8,6 +8,13 @@ class ConTFound4JavaApplicationTests {
 
 	@Test
 	void contextLoads() {
+		String str = "2025-12-22";
+		System.out.println(str.substring(0,4));
+	}
+
+	public static void main(String[] args) {
+		String str = "2025-12-22";
+		System.out.println(str.substring(0,4));
 	}
 
 }
